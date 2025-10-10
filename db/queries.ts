f47d17b158cbda652c5b9f92e63d4d0fbaf75242
phong1675 <23010873@st.phenikaa-uni.edu.vn>
2025-10-11 03:35:05 +0700

    Xong User Progress

--- db/queries.ts
+++ db/queries.ts
@@ -1,9 +1,38 @@
 import { cache } from "react";
 
 import db from "@/db/drizzle";
+import { eq } from "drizzle-orm";
+import { courses, userProgress } from "@/db/schema";
+import { auth } from "@clerk/nextjs/server";
+
+export const getUserProgress = cache(async () => { 
+    const { userId } = await auth();
+
+    if (!userId) {
+        return null;
+    }
+
+    const data = await db.query.userProgress.findFirst({
+        where: eq(userProgress.userId, userId),
+        with: {
+            activeCourse: true,
+        }, 
+    });
+
+    return data;
+});
 
 export const getCourses = cache(async () => {
     const data = await db.query.courses.findMany();
+
+    return data;
+});
+
+export const getCourseById = cache(async (courseId: number) => {
+    const data = await db.query.courses.findFirst({
+        where: eq(courses.id, courseId),
+        //TODO: Populate units and lessons
+    });
 
     return data;
 });
